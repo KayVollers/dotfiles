e1115bbceed4d8d9db1e2ff59287eb51b1ba7dfa
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2015-08-25 10:38:38 +0200

    update karabiner settings

--- karabiner/karabiner-settings.sh
+++ karabiner/karabiner-settings.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set remap.app_finder_return2commandO 1
-$cli set remap.doublepresscommandQ 1
-$cli set repeat.wait 33
 $cli set repeat.initial_wait 300
 $cli set remap.pclikepageupdown_except_emacs 1
-$cli set remap.app_finder_cmd_opt_r_to_return 1
-$cli set remap.pc_application2optionL 1
 $cli set remap.pclikehomeend 1
+$cli set repeat.wait 33
+$cli set remap.app_finder_forwarddelete2commandLdelete 1
+$cli set dk4_media 1
+$cli set remap.doublepresscommandQ 1
+$cli set dk4.media_keys 1
+$cli set remap.app_finder_cmd_opt_r_to_return 1
+$cli set remap.pc_application2optionR 1

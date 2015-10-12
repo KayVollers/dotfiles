eca6037ab6ce8916d635d115385aba52899ca919
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2015-10-12 14:35:17 +0200

    fix git status showing empty stashed changes

--- git/git-status.sh
+++ git/git-status.sh
@@ -5,12 +5,12 @@
 branchName=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 if [[ "$branchName" != "" ]]
 then
-  if [[ "$(git stash list 2>/dev/null | grep $branchName)" != "" ]]
+  if [[ "$(git stash list 2>/dev/null | grep -i "on $branchName")" != "" ]]
   then
     echo
     echo "Stashed changes:"
     echo
-    output=$(git stash list 2>/dev/null | grep -i --color=never "On $branchName")
+    output=$(git stash list 2>/dev/null | grep -i --color=never "on $branchName")
     while read -r line; do
       echo -e "\t\e[0;33m$line\e[0m"
     done <<< "$output"

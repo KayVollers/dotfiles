520d2abb82332c50bbcce20b59db1010b9eb1422
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2016-01-12 15:17:29 +0100

    adapt powerlevel9k config to release v0.3.0

--- zsh/powerlevel9k.zsh
+++ zsh/powerlevel9k.zsh
@@ -7,37 +7,11 @@ POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_DISABLE_RPROMPT=true
 
 # Change some colors
-POWERLEVEL9K_DIR_FOREGROUND='white'
-POWERLEVEL9K_STATUS_BACKGROUND='red'
-
-# Left prompt segments
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time status dir vcs)
+POWERLEVEL9K_DIR_HOME_FOREGROUND='white'
+POWERLEVEL9K_DIR_DEFAULT_FOREGROUND='white'
 
 # Print status segment only if there is an error or a forked job
 POWERLEVEL9K_STATUS_VERBOSE=false
 
-# Overwrite status prompt to print a white fail icon
-# Unfortunately, POWERLEVEL9K_STATUS_FOREGROUND='white' does not work
-prompt_status() {
-  local symbols bg
-  symbols=()
-
-  if [[ "$POWERLEVEL9K_STATUS_VERBOSE" == true ]]; then
-    if [[ "$RETVAL" -ne 0 ]]; then
-      symbols+="%F{226}$RETVAL $(print_icon 'CARRIAGE_RETURN_ICON')%f"
-      bg="red"
-    else
-      symbols+="%F{046}$(print_icon 'OK_ICON')%f"
-      bg="black"
-    fi
-  else
-    # changed from {red} to {white}
-    [[ "$RETVAL" -ne 0 ]] && symbols+="%{%F{white}%}$(print_icon 'FAIL_ICON')%f"
-    bg="$DEFAULT_COLOR"
-  fi
-
-  [[ "$UID" -eq 0 ]] && symbols+="%{%F{yellow}%} $(print_icon 'ROOT_ICON')%f"
-  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{%F{cyan}%}$(print_icon 'BACKGROUND_JOBS_ICON')%f"
-
-  [[ -n "$symbols" ]] && "$1_prompt_segment" "$0" "$bg" "white" "$symbols"
-}
+# Left prompt segments
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(status background_jobs_joined root_indicator_joined time dir vcs)
